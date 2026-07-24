caeff94b4587ffcec7b93d0518c1cf580e089dac
Nelo Mitranim <me@mitranim.com>
2026-07-24 18:38:12 +0200

    Sublime Text support: eval selection

---
+++ sublime/astil_forth.py
@@ -0,0 +1,115 @@
+# BOT-ASSISTED
+
+import sublime_plugin
+import codecs
+import os
+import signal
+import subprocess as sub
+import threading
+
+PLUGIN_NAME = "astil_forth"
+PANEL_NAME = "output." + PLUGIN_NAME # Prefix is required by API.
+READ_SIZE = 64 * 1024
+
+class Eval(threading.Thread):
+  def __init__(self, panel, src, cwd):
+    super().__init__(daemon=True)
+    self.panel = panel
+    self.src = src
+    self.cwd = cwd
+    self.proc = None
+    self.canceled = False
+    self.announce = False
+
+  def cancel(self, announce=False):
+    self.canceled = True
+    self.announce |= announce
+    self.kill()
+
+  def kill(self):
+    proc = self.proc
+    if proc:
+      try:
+        os.killpg(proc.pid, signal.SIGKILL)
+      except ProcessLookupError:
+        pass
+
+  def append(self, text):
+    if text:
+      self.panel.run_command("append", {"characters": text})
+
+  def run(self):
+    try:
+      if self.canceled: return
+
+      self.proc = sub.Popen(
+        args=["astil", "lang.af", "--eval=" + self.src],
+        stdout=sub.PIPE,
+        stderr=sub.STDOUT,
+        cwd=self.cwd,
+        start_new_session=True,
+      )
+
+      if self.canceled:
+        self.kill()
+
+      decoder = codecs.getincrementaldecoder("utf-8")(errors="replace")
+      for chunk in iter(lambda: self.proc.stdout.read1(READ_SIZE), b""):
+        if self.canceled: break
+        self.append(decoder.decode(chunk))
+      if not self.canceled or self.announce:
+        self.append(decoder.decode(b"", final=True))
+    except OSError as err:
+      self.kill()
+      if not self.canceled:
+        self.append(f"{err}\n")
+    finally:
+      if self.proc:
+        self.proc.wait()
+        self.proc = None
+      if self.announce:
+        self.append("[canceled]\n")
+
+class astil_forth_eval_selection(sublime_plugin.WindowCommand):
+  eval = None
+
+  def run(self, cancel=False):
+    if cancel:
+      self.cancel(True)
+      return
+
+    view = self.window.active_view()
+    if not view: return
+
+    for reg in view.sel():
+      src = view.substr(reg)
+      if not src: continue
+
+      panel = self.window.create_output_panel(PLUGIN_NAME)
+      self.window.run_command("show_panel", {"panel": PANEL_NAME})
+      self.queue_eval(panel, src, guess_cwd(view))
+
+  def queue_eval(self, panel, src, cwd):
+    self.cancel()
+    self.eval = Eval(panel, src, cwd)
+    self.eval.start()
+
+  def cancel(self, announce=False):
+    if self.eval:
+      self.eval.cancel(announce)
+      self.eval = None
+
+class astil_forth_event_listener(sublime_plugin.EventListener):
+  def on_window_command(self, window, command_name, args):
+    if command_name == "hide_panel" and window.active_panel() == PANEL_NAME:
+      window.run_command("astil_forth_eval_selection", {"cancel": True})
+
+  def on_pre_close_window(self, window):
+    window.run_command("astil_forth_eval_selection", {"cancel": True})
+
+def guess_cwd(view):
+  window = view.window()
+  if view.file_name():
+    return os.path.dirname(view.file_name())
+  if len(window.folders()):
+    return window.folders()[0]
--- sublime/readme.md
+++ sublime/readme.md
@@ -13,3 +13,23 @@ ln -sfn "$(pwd)/sublime" "$HOME/Library/Application Support/Sublime Text/Package
 ```
 
 To find the packages directory on your system, use Sublime Text menu → Preferences → Browse Packages.
+
+## Run file
+
+This package provides an ST "build system": a shortcut for running the current file using the Astil Forth interpreter. Works out of the box in `*.af` files. Hit ST's built-in hotkey ⌘B to run in default mode, or ⌘⇧B to select between variants.
+
+## Eval selection
+
+This package also provides the command ⌘⇧P → "Astil Forth: eval selection", which runs selected code using `astil` as subprocess. Example keybind configuration; choose your own keys:
+
+```json
+{
+  "keys": ["alt+ctrl+b"],
+  "command": "astil_forth_eval_selection",
+  "context": [{"key": "selector", "operand": "source.astil_forth"}],
+}
+```
+
+The subprocess can be killed via Ctrl+C or simply by closing the output panel.
+
+The command is opt-in. Our Python plugin doesn't do anything else.
